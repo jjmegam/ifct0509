15fed5e7ccd96b1441987af32057218b9285da1d
jjmegam <jjmegam@hotmail.com>
2015-03-04 20:54:47 +0100

    juanjo

--- P0ker.php
+++ P0ker.php
@@ -1,6 +1,6 @@
-<?php 
-				       
-
+<?php 	
+     				$carta1 = "<img src = dorso1.png>";
+				  	$carta2 = "<img src = dorso1.png>";
 
 						if(isset($_POST["dar"])){
 
@@ -9,11 +9,8 @@
 
 				  $carta1 = "<img src = carta_" . $c1 . "_" . $p1 . ".png>";
 				  $carta2 = "<img src = carta_" . $c2 . "_" . $p2 . ".png>";			  
-
-}
-
+												}
  ?>
-
 
 <?php include '_header.php' ?>
 
@@ -21,14 +18,10 @@
 
 		    			<div class= "well">
 		    				<h3>JUGADA</h3>
-		    				<?php echo $carta1 , $carta2 ?>
+		    				<?php echo $carta1 . ", ," . $carta2 ?>
 		    			</div>
 		    		
-			<?php 	
-					$carta1="<img src ="dorso.png">";
-					$carta2="<img src ="dorso.png">";
 
-			echo $carta1 , $carta2 ?>
  			<form action="P0ker.php" method="post">
 			
 		    	<input type="submit" name="dar" />
